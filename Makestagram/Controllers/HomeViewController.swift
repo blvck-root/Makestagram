be4667698f8c820dfdbf7544d285218e28c5b982
Mthabisi Ndlovu <mndlovu@usapglobal.org>
2017-09-02 22:48:42 +0000

    Makestagram 16
    Implemented the timeline.

--- Makestagram/Controllers/HomeViewController.swift
+++ Makestagram/Controllers/HomeViewController.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    var posts = [Post]()
     
+    // MARK: - Subviews
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - VC Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UserService.posts(for: User.current) { (posts) in
+            self.posts = posts
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
+        cell.backgroundColor = .black 
+        
+        return cell
+    }
 }
--- Makestagram/Models/Post.swift
+++ Makestagram/Models/Post.swift
@@ -18,8 +18,21 @@ class Post {
         let createdAgo = creationDate.timeIntervalSince1970
         
         return ["image_url" : imageURL,
-                "image_Height" : imageHeight,
+                "image_height" : imageHeight,
                 "created_at": createdAgo]
+    }
+    
+    init?(snapshot: DataSnapshot) {
+        guard  let dict = snapshot.value as? [String: Any],
+            let imageURL = dict["image_url"] as? String,
+            let imageHeight = dict["image_height"] as? CGFloat,
+            let createdAgo = dict["created_at"] as? TimeInterval
+            else { return nil }
+        
+        self.key = snapshot.key
+        self.imageURL = imageURL
+        self.imageHeight = imageHeight
+        self.creationDate = Date(timeIntervalSince1970: createdAgo)
     }
     
     init(imageURL: String, imageHeight: CGFloat) {
