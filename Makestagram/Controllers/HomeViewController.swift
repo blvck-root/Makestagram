b3740fe21febd3c6cdb28189f0c1f37c0262d55d
Mthabisi Ndlovu <mndlovu@usapglobal.org>
2017-09-03 14:33:37 +0000

    Implemented downloading photos for each post and displaying them on the timeline.
    Makestagram 17

--- Makestagram/Controllers/HomeViewController.swift
+++ Makestagram/Controllers/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class HomeViewController: UIViewController {
     var posts = [Post]()
@@ -24,6 +25,15 @@ class HomeViewController: UIViewController {
             self.posts = posts
             self.tableView.reloadData()
         }
+        
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        // remove  separators for empty cells
+        tableView.tableFooterView = UIView()
+        // remove separators from cells
+        tableView.separatorStyle = .none
     }
 }
 
@@ -33,9 +43,20 @@ extension HomeViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
-        cell.backgroundColor = .black 
+        let post = posts[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath) as! PostImageCell
+        
+        let imageURL = URL(string: post.imageURL)
+        cell.postImageView.kf.setImage(with: imageURL)
         
         return cell
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let post = posts[indexPath.row]
+        
+        return post.imageHeight
     }
 }
---
+++ Makestagram/Views/PostImageCell.swift
@@ -0,0 +1,22 @@
+//
+//  PostImageCell.swift
+//  Makestagram
+//
+//  Created by El Capitan on 9/3/17.
+//  Copyright © 2017 Mthabisi Ndlovu. All rights reserved.
+//
+
+import UIKit
+
+class PostImageCell: UITableViewCell {
+    
+    // MARK: - Subviews
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    // MARK: - VC Lifecycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
